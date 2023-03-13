07b23086543e811d831d0553aab466141a5fce30
RubenMillan95 <necroptich@gmail.com>
2023-03-12 23:29:54 -0500

    version 0.2

---
+++ factorial.js
@@ -0,0 +1,21 @@
+
+function factorial(){
+    let n=parseInt(document.getElementById('n')); //con esto vinculo con el tag
+    let factorial=1;
+    let i=2;
+
+//ciclos aguas!!
+//while indefinidas veces el uso de esta funcion
+// for para saber cuantas veces se repite algo
+
+while(i<=n){
+    factorial=factorial*1;
+    i=i+1;  //ó i++ ó i+=1
+}
+return factorial;
+
+
+
+
+
+}
